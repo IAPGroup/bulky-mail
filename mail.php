b85a1960c28e83f87ba53d7b58d199b0ebddc900
NjekeMbai <73385119+NjekeMbai@users.noreply.github.com>
2020-11-19 12:17:12 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
 
 require_once('./vendor/autoload.php');
 
-$mailchimp = new MailchimpMarketing\ApiClient();
+$mailchimp = new MailchimpTransactional\ApiClient();
 
 $mailchimp->setConfig([
   'apiKey' => '9ff0c0dc2553f9eab0f862d139113b36-us7',
